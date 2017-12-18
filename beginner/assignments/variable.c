d20161eccbcb4fc8bd0d4779febfecc01ef82cb2
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2017-12-18 15:18:45 +0530

    Create variable.c

---
+++ beginner/assignments/variable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ char name[]={"Sachin Ramesh Tendulkar"};
+ char nickn[]={"Little Master"};
+ int run=18426;
+ int avg=44.83;
+ int totmatch=463;
+ printf("%s",name);
+ printf("\n%s",nickn);
+ printf("\n%d",run);
+ printf("\n%d",avg);
+ printf("\n%d",totmatch);
+ return 0;
+ }
+ 
