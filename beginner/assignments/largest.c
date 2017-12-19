119ecab4d3f790a56a7854a3579bccbfbad6396c
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2017-12-19 06:43:42 +0530

    Update largest.c

--- beginner/assignments/largest.c
+++ beginner/assignments/largest.c
@@ -2,14 +2,13 @@
 int main()
 {
 int a,b,c;
-scanf("%d %d %d",&a,&b,&c);
-if(a>b>c)
+scanf("%d\n %d\n %d",&a,&b,&c);
+if(a>b&&a>c)
 printf("\n%d is largest",a);
-else if(b>c>a)
+else if(b>c&&b>a)
 printf("\n%d is largest",b);
-else if(c>a>b)
+else if(c>a&&c>b)
 printf("\n%d is largest",c);
-else
-printf("\nall are equal");
+
 return 0;
 }
