a6f0b82f4286ab465535b1ba98b2988e6c983143
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2017-12-20 16:07:24 +0530

    Update rotationofarray.c

--- codekata/rotationofarray.c
+++ codekata/rotationofarray.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+int a[10];
+int n,k,i,j,temp;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	scanf("%d",&a[i]);
+	
+}
+scanf("%d",&k);
+while(k>0)
+{    temp=a[0];
+	for(i=0,j=i+1;i<n&&j<n;i++,j++)
+	{
+	a[i]=a[j];
+		 	
+	}
+	a[i]=temp;
+	k--;
+}
+for(i=0;i<n;i++)
+{
+	printf("%d",a[i]);
+}
+	return 0;
+}
 
