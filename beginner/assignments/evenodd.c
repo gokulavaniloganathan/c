e107b4ccaf6f6bf8e43bdafe7c247df7293b1be4
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2017-12-20 22:25:40 +0530

    Update evenodd.c

--- beginner/assignments/evenodd.c
+++ beginner/assignments/evenodd.c
@@ -2,10 +2,10 @@
 int main()
 {
 int a;
-scanf("%d",&a);
-if(a%2==0)
-printf("even");
-else
-printf("odd");
-return 0;
+  scanf("%d",&a);
+  if(a%2==0)
+    printf("even");
+  else
+    printf("odd");
+  return 0;
 }
