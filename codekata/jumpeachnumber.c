97a0f8f40b2de9c0773492b0f1202f2ab67c7c8a
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2017-12-21 15:39:43 +0530

    Update jumpeachnumber.c

--- codekata/jumpeachnumber.c
+++ codekata/jumpeachnumber.c
@@ -1,1 +1,24 @@
-
+#include<stdio.h>
+int main(void)
+{
+  int a[10];
+  int i,j,n,flag=0;
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i+=a[i])
+  {
+    if(i==n-1)
+    {
+      flag=1;
+      printf("true");
+    }
+  }
+  if(flag==0)
+  {
+printf("false");
+  }
+  return 0;
+}
