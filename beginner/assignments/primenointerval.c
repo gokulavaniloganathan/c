7e8efa2e09270192213e415d4bc7843cbe427f7a
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2018-01-19 16:04:41 +0530

    Add files via upload

---
+++ beginner/assignments/primenointerval.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,b,count=0,i,j;
+scanf("%d %d",&a,&b);
+for(i=a+1;i<b;i++)
+{ count=0;
+  for(j=1;j<=i;j++)
+{
+ if(i%j==0)
+ count++;
+}
+ if(count==2)
+ printf("%d ",i);
+}
+return 0;
+} 
