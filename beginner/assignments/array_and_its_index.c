c406c554d163c4af3eca71e7912b3f682efb9bc8
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2018-01-20 22:11:10 +0530

    Add files via upload

---
+++ beginner/assignments/array_and_its_index.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+ int n;
+scanf("%d\n",&n);
+int a[n];
+int i;
+for(i=0;i<n;i++)
+{
+ scanf("%d ",&a[i]);
+ }
+for(i=0;i<n;i++)
+{
+ printf("%d %d\n",a[i],i);
+}
+return 0;
+}
