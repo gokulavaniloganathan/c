c331642c6f7699403079e06b365c04619c077b1e
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2018-02-24 11:25:17 +0530

    Add files via upload

--- beginner/assignments/countdigit.c
+++ beginner/assignments/countdigit.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 int main()
 {
-int a,n,count=0;
-scanf("%d",&n);
-while(n>0)
-{count++;
- a=n%10;
- n=n/10;
- }
- printf("%d",count);
- return 0;
- }
- 
+	long long int n;
+	scanf("%lld",&n);
+	long long int i,c;
+	int count=0;
+	while(n!=0)
+	{
+		c=n%10;
+		n=n/10;
+		count++;
+	}
+	printf("%d",count);
+	return 0;
+}
