099dbcc34493a502ba5b0931915e1c83a7812486
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2018-02-24 18:41:35 +0530

    Update swap_without_bitwise.c

--- beginner/assignments/swap_without_bitwise.c
+++ beginner/assignments/swap_without_bitwise.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-	int n,n1,temp;
-	scanf("%d %d",&n,&n1);
+	long int n,n1,temp;
+	scanf("%ld %ld",&n,&n1);
 	temp=n;
 	n=n1;
 	n1=temp;
-	printf("%d %d",n,n1);
+	printf("%ld %ld",n,n1);
 	return 0;
 }
