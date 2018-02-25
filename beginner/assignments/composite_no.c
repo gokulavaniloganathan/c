0d0458c28f804f5acbad2bef906080c95819988c
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2018-02-25 16:08:51 +0530

    Add files via upload

---
+++ beginner/assignments/composite_no.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int i,count=0;
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		count++;
+		
+	}
+	if(count>2)
+	{
+		printf("yes");
+	}
+	else
+	printf("no");
+	return 0;
+}
