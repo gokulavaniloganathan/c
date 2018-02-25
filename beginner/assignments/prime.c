133d10767897fc450765f678d86d17acf0817da3
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2018-02-25 11:59:45 +0530

    Add files via upload

--- beginner/assignments/prime.c
+++ beginner/assignments/prime.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 int main()
 {
-int a,i,count=0;
-scanf("%d",&a);
-for(i=1;i<=a;i++)
-{
- if(a%i==0)
- count++;
-}
-if(count==2)
- printf("yes");
-else
-printf("no");
-return 0;
+	int n,i,count=0;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		count++;
+	}
+	if(count>2)
+	{
+		printf("no");
+		
+	}
+	else 
+	printf("yes");
+	return 0;
 }
