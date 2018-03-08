9243739fff6cecb149e771367fd0b9879d287938
gokulavaniloganathan <34640959+gokulavaniloganathan@users.noreply.github.com>
2018-03-08 09:58:28 +0530

    Add files via upload

---
+++ beginner/assignments/lexicographical_order.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int main()
+
+{
+char s[100],temp;
+
+scanf("%s",s);
+
+int i,j;
+
+for(i=0;s[i]!='\0';i++)
+
+{
+	
+for(j=i+1;s[j]!='\0';j++)
+
+{
+	if(s[i]>s[j])
+
+	{
+	temp=s[i];
+
+		s[i]=s[j];
+	
+               s[j]=temp;
+	
+         }
+
+}
+
+}
+
+printf("%s",s);
+
+return 0;
+
+}
